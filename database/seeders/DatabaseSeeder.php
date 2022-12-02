a062c5bb24f9e4dbb35535655de8f2ba4601cbf0
Dani <d.tarazoma@geekshubs.com>
2022-12-02 12:35:36 +0100

    feat: TaskSeeder added to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Log;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +21,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Log::info('Esto es el DatabaseSeeder');
+        
+        $this->call([
+            TaskSeeder::class,
+        ]);
     }
 }
